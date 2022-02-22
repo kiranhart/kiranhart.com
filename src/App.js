760e12eb5892919997d9aeb138ae330e66dbe2e8
Kiran Hart <kiran.hart@live.com>
2022-02-22 01:19:42 -0500

    clear

--- src/App.js
+++ src/App.js
@@ -1,33 +1,9 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { useSelector } from 'react-redux';
-
-import { ThemeProvider } from 'styled-components';
-import { lightTheme, darkTheme, GlobalStyles } from './theme';
-
-import ThemeToggle from './components/ThemeToggle';
-import Navbar from './components/Navbar';
-import Home from './pages/Home';
-import About from './pages/About';
-import Projects from './pages/Projects';
-import Contact from './pages/Contact';
 
 const App = () => {
-    const isDark = useSelector((state) => state.theme.theme) === 'dark';
-
-    return <ThemeProvider theme={isDark ? darkTheme : lightTheme}>
-        <BrowserRouter>
-            <GlobalStyles />
-            <Navbar />
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/about" element={<About />} />
-                <Route path="/projects" element={<Projects />} />
-                <Route path="/contact" element={<Contact />} />
-            </Routes>
-            <ThemeToggle />
-        </BrowserRouter>
-    </ThemeProvider>;
+    return <div className="min-h-screen bg-slate-900 text-white flex items-center justify-center">
+        <h1 className="text-6xl font-bold">Let's Begin</h1>
+    </div>;
 };
 
 export default App;
--- src/store/reducers/index.js
+++
@@ -1,6 +0,0 @@
-import { combineReducers } from 'redux';
-import themeReduer from './themeReducer';
-
-export default combineReducers({
-    theme: themeReduer
-});
